739d107b989ffe2fb32e89e53a14fb2bb23145d1
Vitor A Thomazini <vitor.a.thomazini@proton.me>
2023-11-03 08:13:15 -0300

    refactor: change name package 'common' to '@shared'

--- source/src/domain/repository/customer-repository.interface.ts
+++ source/src/domain/repository/customer-repository.interface.ts
@@ -1,8 +1,8 @@
 import { Customer } from "../entity/customer";
-import { CreateRepositoryInterface } from "./common/create-repository.interface";
-import { FindAllRepositoryInterface } from "./common/find-all-repository.interface";
-import { FindRepositoryInterface } from "./common/find-repository.interface";
-import { UpdateRepositoryInterface } from "./common/update-repository.interface";
+import { CreateRepositoryInterface } from "./@shared/create-repository.interface";
+import { FindAllRepositoryInterface } from "./@shared/find-all-repository.interface";
+import { FindRepositoryInterface } from "./@shared/find-repository.interface";
+import { UpdateRepositoryInterface } from "./@shared/update-repository.interface";
 
 export interface CustomerRepositoryInterface 
   extends CreateRepositoryInterface<Customer>, 
--- source/src/domain/repository/product-repository.interface.ts
+++ source/src/domain/repository/product-repository.interface.ts
@@ -1,8 +1,8 @@
 import { Product } from "../entity/product";
-import { CreateRepositoryInterface } from "./common/create-repository.interface";
-import { FindAllRepositoryInterface } from "./common/find-all-repository.interface";
-import { FindRepositoryInterface } from "./common/find-repository.interface";
-import { UpdateRepositoryInterface } from "./common/update-repository.interface";
+import { CreateRepositoryInterface } from "./@shared/create-repository.interface";
+import { FindAllRepositoryInterface } from "./@shared/find-all-repository.interface";
+import { FindRepositoryInterface } from "./@shared/find-repository.interface";
+import { UpdateRepositoryInterface } from "./@shared/update-repository.interface";
 
 export interface ProductRepositoryInterface 
   extends CreateRepositoryInterface<Product>, 
